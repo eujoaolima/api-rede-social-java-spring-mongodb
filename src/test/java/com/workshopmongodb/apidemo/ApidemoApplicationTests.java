49ca29f67573d73a38d56e661d6e043a7017adaf
eujoaolima <joaopedro.donatolima@hotmail.com>
2023-09-05 11:23:21 -0300

    first commit

---
+++ src/test/java/com/workshopmongodb/apidemo/ApidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshopmongodb.apidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
