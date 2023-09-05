7904ec8a942214b21bd22d61d513561e6d9e5a22
eujoaolima <joaopedro.donatolima@hotmail.com>
2023-09-05 17:34:26 -0300

    Consulta simples com @Query

--- src/main/java/com/workshopmongodb/apidemo/repository/PostRepository.java
+++ src/main/java/com/workshopmongodb/apidemo/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.workshopmongodb.apidemo.repository;
 import com.workshopmongodb.apidemo.entity.Post;
 import com.workshopmongodb.apidemo.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-    List<Post> findByTitleContaining(String text);
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
